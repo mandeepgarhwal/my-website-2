6ae872052b06d44cc32d395c05c157e167896dbc
mandeepgarhwal <mandeepgarhwal@gmail.com>
2023-05-07 06:25:20 +0530

    a

---
+++ assignment 15/assignment15.js
@@ -0,0 +1,112 @@
+// changing paragraph color using getelementsbytagNmae
+// var x = document.getElementsByTagName("p")
+// console.log(x)
+// x[0].innerHTML = "This color is changed using getElementsByTagname"
+// x[0].style.color = "red"
+
+// changing paragraph color using getelementsbyClassName
+// var x = document.getElementsByClassName("paragraphs")
+// console.log(x)
+// x[1].innerHTML = "This color is changed using getElementsByClassName"
+// x[1].style.color = "pink"
+
+// changing paragraph color using getelementById
+// var x = document.getElementById("para1")
+// console.log(x)
+// x.innerHTML = "This color is changed using getElementById"
+// x.style.fontSize = "30px"
+
+// changing paragraph color using queryselctor
+// var x = document.querySelector("p")
+// console.log(x)
+// x.innerHTML = "This is changed using queryselector"
+// x.style.fontSize = "30px"
+
+// changing paragraph color using queryselctorAll
+// var x = document.querySelectorAll("p")
+// console.log(x)
+// x[1].innerHTML = "This is changed using queryselectorAll"
+// x[1].style.fontSize = "30px"
+
+
+// changing heading text using queryselctor
+// var x = document.querySelector("#heading1")
+// console.log(x)
+// x.innerHTML = "This is changed using queryselector"
+
+// changing heading text using queryselctorAll
+// var x = document.querySelectorAll(".headings")
+// console.log(x)
+// x[1].innerHTML = "This is changed using queryselectorAll"
+
+// changing heading text using getelementsbytagname
+// var x = document.getElementsByTagName("h1")
+// console.log(x)
+// x[0].innerHTML = "This is changed using getElementsByTagname"
+
+
+// changing heading text using getelementsByClassname
+// var x = document.getElementsByClassName("headings")
+// console.log(x)
+// x[1].innerHTML = "This is changed using getElementsByClassname"
+
+
+// changing heading text using getelementById
+// var x = document.getElementById("heading2")
+// console.log(x)
+// x.innerHTML = "This is changed using getElementById"
+
+// changing background color of div using id
+
+// var x = document.getElementById("div1")
+// console.log(x)
+// x.style.backgroundColor = "pink"
+
+// changing background color of div using tagname
+// var x = document.getElementsByTagName("div")
+// console.log(x)
+// x[1].style.backgroundColor = "red"
+
+// changing background color of div using class
+// var x = document.getElementsByClassName("divs")
+// console.log(x)
+// x[0].style.backgroundColor = "blue"
+
+// changing background color of div using queryselctor
+// var x = document.querySelector(".divs")
+// console.log(x)
+// x.style.backgroundColor = "green"
+
+// changing background color of div using queryselctorAll
+// var x = document.querySelectorAll(".divs")
+// console.log(x)
+// x[1].style.backgroundColor = "green"
+
+// arranging div horizontally using id
+// var x = document.getElementById("body")
+// console.log(x)
+// x.style.display = "flex"
+
+// arranging div horizontally using tag name
+// var x = document.getElementsByTagName("body")
+// console.log(x)
+// x[0].style.display = "flex"
+
+// arranging div horizontally using Class name
+// var x = document.getElementsByClassName("mainbody")
+// console.log(x)
+// x[0].style.display = "flex"
+
+// arranging div horizontally using queryselectorAll
+// var x = document.querySelectorAll("body")
+// console.log(x)
+// x[0].style.display = "flex"
+
+// arranging div horizontally using queryselector
+var x = document.querySelector("body")
+console.log(x)
+x.style.display = "flex"
+
+
+
+
